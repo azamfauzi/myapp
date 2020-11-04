e57a6a95cd6faba57f9192f2cc2df5cb1ebf8b8e
azamfauzi <ibaneznuke@gmail.com>
2020-11-04 22:27:48 +0800

    Update index.pp

--- src/index.php
+++ src/index.php
@@ -2,5 +2,6 @@
 <?php echo "hello worl";?>
 <?php echo "myworl is there";?>
 <?php echo "hehhehe"?>
-<?php echo "hello world in the world";?>:w
+<?php echo "hello world in the world";?>
+<?php eho "thei new work d" ?>
 
